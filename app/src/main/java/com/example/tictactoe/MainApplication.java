2cfac60739fff7691f4aef728a14ab4a09de3e1f
Jan <jan.m.tennert@gmail.com>
2024-02-10 14:00:10 +0100

    add dynamic colors

---
+++ app/src/main/java/com/example/tictactoe/MainApplication.java
@@ -0,0 +1,13 @@
+package com.example.tictactoe;
+
+import android.app.Application;
+
+import com.google.android.material.color.DynamicColors;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(this);
+    }
+}
